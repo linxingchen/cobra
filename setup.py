7a01a6307ef98c6207dac33646dce47f6cf1829b
LinXing Chen <linkingchan@gmail.com>
2023-09-03 18:08:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="cobra",
-    version="1.1.54",
-    description="a bioinformatics tool to get higher quality viral genomes assembled from metagenomes of short paired-end reads",
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/linxingchen/cobra.github.io",
-    author="Linxing Chen",
-    author_email="linkingchan@gmail.com",
-    license="MIT",  # or other licenses you prefer
-    classifiers=[
-        "Development Status :: 3 - Alpha",  # or "5 - Production/Stable" or another status
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",  # matching the license from above
-        "Programming Language :: Python :: 3",  # or "2.7", "3.6", etc.
+    name='cobra-meta',
+    version='1.2.2',
+    py_modules=['cobra'],
+    install_requires=[
+        'biopython',
+        'pysam'
     ],
-    python_requires=">=3.7",  # your package's Python version requirements
+    author='LINXING CHEN',
+    description='COBRA (Contig Overlap Based Re-Assembly) is a bioinformatics tool to get higher quality viral genomes assembled from metagenomes of short paired-end reads. COBRA was written in Python. COBRA has so far only been tested on assembled contigs from metaSPAdes, IDBA_UD, and MEGAHIT.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/linxingchen/cobra',
+    entry_points={
+        'console_scripts': [
+            'cobra-meta=cobra:main',
+        ],
+    },
 )
